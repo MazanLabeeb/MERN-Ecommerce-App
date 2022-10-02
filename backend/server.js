001b55357d26677ad1b6c995f695e1c0dbf8bcbe
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-02 19:32:31 +0500

    001 API added

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = new express();
+const PORT = process.env.PORT || 8080;
+const {products} = require("./data/products");
+
+// ROUTES
+app.get('/', (req, res)=>{
+    res.send("Forbidden");
+})
+
+app.get('/products', (req, res)=> {
+    res.json(products);
+})
+
+app.get('/products/:id', (req, res)=> {
+    const filterProduct = products.filter(id=> id._id=== req.params.id );
+    res.json(filterProduct);
+})
+
+
+app.use((req, res)=>{
+    res.send("Error 404! Page Not Found");
+})
+
+app.use((err, req, res, next)=>{
+    res.status(500).send(`Error 500! Something went wrong.<br><b>Error: </b>${err.stack}`);
+})
+
+
+
+
+const onStartHttp = () => {
+    console.log(`Server Started: http://localhost:${PORT}`);
+}
+app.listen( PORT, onStartHttp );
