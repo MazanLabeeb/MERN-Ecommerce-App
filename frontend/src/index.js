9e63f1cb4b37e398eb609f5dfbb943b43826fbac
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-10-12 23:11:58 +0500

    redux configuration done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,15 @@ import "./bootstrap.min.css";
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ frontend/src/store/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import logger from "redux-logger";
+import { rootReducer } from "./rootReducer";
+import thunk from "redux-thunk";
+
+const middlewares = [process.env.NODE_ENV === "development" && logger, thunk].filter(Boolean);
+
+const ChromeDevTools = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const enhancedCompose = ChromeDevTools(applyMiddleware(...middlewares));
+
+
+const initialState = {};
+
+export const store = createStore(rootReducer, initialState, enhancedCompose);
